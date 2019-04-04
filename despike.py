8cb363153f67034f778690e2e6fbbd2f6d861d24
Leach <thenickleach@gmail.com>
2019-04-04 10:52:34 -0700

    Thresholding for despike improved

--- despike.py
+++ despike.py
@@ -5,7 +5,7 @@ Purpose: remove spikes from a dataset
 import numpy as np
 import os
 
-def despike(vector, threshold, known_spikes=[]):
+def despike(vector, threshold, known_spikes=None):
     """
     IMPORTANT - has not been tested with known spikes!!  Suspect there are errors in that section of code.
     Only despikes one vector currently. Must be run on each band individually
@@ -22,8 +22,9 @@ def despike(vector, threshold, known_spikes=[]):
         vec = np.loadtxt(vec)
 
     # If an array containing known spikes is given, only despike those locations
-    if known_spikes != []:
-        spike_locations = np.loadtxt(known_spikes, delimiter=',')
+    if type(known_spikes) == np.ndarray:
+        spike_locations = known_spikes
+        #spike_locations = np.loadtxt(known_spikes, delimiter=',')
         trozo = spike_locations[1:len(spike_locations)-2]       # suspect that this line is bad. may need -1 instead
         # intializing other arrays to match trozo
         spike0 = trozo
@@ -42,15 +43,16 @@ def despike(vector, threshold, known_spikes=[]):
     else:
         left = vec[0:len(vec)-2]
         right = vec[2:len(vec)]
-        spike = abs(vec[1:len(vec)-1]-((left+right)/2.))        # could use better indexing to make this more pythonic
+        spike = abs(vec[1:len(vec)-1]-((left+right)/2.))        # magnitude of the spikes
         spike0 = spike
-        dif = abs(left-right)/(spike+0.0000000000001)
-        # TODO: check this next line since it looks very weird
-        trozo = (dif < (1-threshold))*(spike != 0.)  # if the difference < 1-threshold, mark as binary spike, unless 'spike' is 0 there??
+        similarity = abs(left-right)/(spike+0.0000000000001)  # similarity of subsequent points
+        standardized_sim = similarity/np.std(similarity)
+        new_thresh = np.max(standardized_sim)*threshold  # 1 despikes everything, 0 despikes nothing
+        trozo = (similarity < new_thresh * (spike != 0.))  # if the difference < 1-threshold, mark as binary spike, unless 'spike' is 0 there??
 
-        trozi = trozo*0
-
-        while np.sum(trozo) != 0:
+        trozi = trozo*0  # make an empty array
+        iteration = 0
+        while (np.sum(trozo) != 0) and (iteration < 1000):  # while there are spikes
             indexMAX = np.argmax(spike*trozo)  # need to assign 'indexMAX' the location of the largest value of spike*trozo
 
             vec[indexMAX+1] = np.mean([vec[indexMAX], vec[indexMAX+2]])
@@ -60,12 +62,17 @@ def despike(vector, threshold, known_spikes=[]):
             left = vec[0:len(vec)-3]
             right = vec[2:len(vec)-1]
             spike = abs(vec[1:len(vec)-2] - ((left+right)/2.))
-            dif = abs(left-right)/(spike+0.0000000000001)
-            trozo = (dif < (1. - threshold)) * (spike != 0.)  # if the difference < 1-threshold, mark as binary spike, unless 'spike' is 0 there??
+            similarity = abs(left-right)/(spike+0.0000000000001)
+            standardized_sim = similarity / np.std(similarity)
+            new_thresh = np.max(standardized_sim) * threshold
+            trozo = (similarity < new_thresh * (spike != 0.))  # if the difference < 1-threshold, mark as binary spike, unless 'spike' is 0 there
+            # this method flags values with small "similarity" as spikes
+            iteration += 1
 
     trozo = trozi
-    if known_spikes != []:
-        spike_locations = np.loadtxt(known_spikes, delimiter=',')
+    if type(known_spikes) == np.ndarray:
+        #spike_locations = np.loadtxt(known_spikes, delimiter=',')
+        spike_locations = known_spikes
         trozo = np.append(trozo, spike_locations[len(spike_locations)-1])
         trozo = np.insert(trozo, 0, 0)
         if spike_locations[len(spike_locations)-1] == 1:
@@ -85,7 +92,7 @@ def despike(vector, threshold, known_spikes=[]):
 if __name__ == '__main__':
     # expects files to be despiked to be .CSV
     location = eval(input("Name of directory or file to despike: "))
-    threshold = float(eval(input("Despike threshold (between 0 and 1): ")))
+    threshold = float(eval(input("Despike threshold (float 0-1, must be <1.0): ")))
     spikes = eval(input("Are there known spike locations? y/n: "))
     if spikes == 'y':
         spikes = eval(input("File path of file with spikes: "))
